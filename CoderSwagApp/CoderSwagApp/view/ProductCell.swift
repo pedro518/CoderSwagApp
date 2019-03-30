5dad058e9552690d66f0f3cf5619a32fb03b40f6
Pedro Antonio Ruiz Cuesta <pruiz@Pedros-iMac.local>
2019-03-30 03:49:27 -0700

    Se añade nuevo VC con UiCollectionView y se crea el protype de su celda

---
+++ CoderSwagApp/CoderSwagApp/view/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  CoderSwagApp
+//
+//  Created by Pedro Antonio Ruiz Cuesta on 30/03/2019.
+//  Copyright © 2019 Pedro Antonio Ruiz Cuesta. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
